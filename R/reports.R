a7edff09653d2f01d01399b2353ad78f0c1f8bce
Matthew Heun <matthew.heun@me.com>
2023-09-08 14:22:27 -0400

    Building a function that makes reports.

---
+++ R/reports.R
@@ -0,0 +1,15 @@
+#' Create a series of efficiency reports
+#'
+#' Creates reports of efficiencies for machines in PSUT data frames.
+#' Each report contains efficiencies for a single machine
+#' across all countries.
+#'
+#' @param eta_i_df A PSUT data frame.
+#'
+#' @return Nothing. This function should be called for its side effect of
+#'         creating reports.
+#'
+#' @export
+create_eta_i_reports <- function(eta_i_df, reports_folder) {
+
+}
