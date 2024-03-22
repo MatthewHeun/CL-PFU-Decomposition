04114c112b491041be85ec9478e890e0e321a65f
Matthew Heun <matthew.heun@me.com>
2024-03-22 13:10:09 -0400

    Update meeting notes and script.

--- Results/IEADetails/iea_details_script.R
+++ Results/IEADetails/iea_details_script.R
@@ -1,8 +1,8 @@
 # This script was used to generate data for work with Zeus and Matt.
 # We're investigating details of specified IEA data.
-# In particular, 
+# In particular,
 # Cuba (Biogasoline),
-# Austria, Switzerland, Sweden, Turkey, USA (Non-specified primary biofuels and waste). 
+# Austria, Switzerland, Sweden, Turkey, USA (Non-specified primary biofuels and waste).
 # All countries (Manufacture [of Other sources], Other sources, Non-specified, Transfers)
 # This script should be run from a place where we can get the SpecifiedIEA data frame.
 # At this time, that is the PFUPipeline project.
@@ -10,25 +10,27 @@
 specified_iea_data <- tar_read(SpecifiedIEA)
 
 # Cuba biogasoline
-cuba_biogasoline <- specified_iea_data |> 
+cuba_biogasoline <- specified_iea_data |>
   dplyr::filter(Country == "CUB", startsWith(Product, "Biogasoline"))
-cuba_biogasoline |> 
+cuba_biogasoline |>
   write.csv("~/Desktop/IEADetails/cuba_biogasoline.csv", row.names = FALSE)
 
 # Non-specified primary biofuels and waste
-nspbw <- specified_iea_data |> 
-  dplyr::filter(Country %in% c("AUT", "CHE", "SWE", "TUR", "USA"), 
+nspbw <- specified_iea_data |>
+  dplyr::filter(Country %in% c("AUT", "CHE", "SWE", "TUR", "USA"),
                 startsWith(Product, "Non-specified primary biofuels and waste"))
-nspbw |> 
+nspbw |>
   write.csv("~/Desktop/IEADetails/Non-specified primary biofuels and waste.csv", row.names = FALSE)
 
 # Other sources
-other_sources <- specified_iea_data |> 
+other_sources <- specified_iea_data |>
   dplyr::filter(startsWith(Product, "Other sources"))
-other_sources |> 
+other_sources |>
   write.csv("~/Desktop/IEADetails/Other sources.csv", row.names = FALSE)
 
-# Non-specified
-non_specified <- specified_iea_data |> 
-  dplyr::filter(Country == "USA", 
-                (startsWith(Product, "Non-specified") | Flow == "Non-specified")) |> View()
+# Non-specified flows
+non_specified <- specified_iea_data |>
+  dplyr::filter(grepl("specified", .data[["Flow"]], fixed = TRUE))
+non_specified |>
+  write.csv("~/Desktop/IEADetails/Non-specified flows.csv", row.names = FALSE)
+
